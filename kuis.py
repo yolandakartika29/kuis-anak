27c96f3cf2f9a0aa13dd81d0e06af9aa7d20ce27
yolandakartika29 <yolandakartikadewi74@gmail.com>
2026-09-23 13:38:41 +0700

    Update kuis.py

--- kuis.py
+++ kuis.py
@@ -55,7 +55,7 @@ with tab1:
                         """
 
                         response = client.models.generate_content(
-                            model="gemini-2.5-flash",
+                            model="gemini-3.6-flash",
                             contents=[image, prompt]
                         )
 
